4dfe3713f20817f29f70598b5f14759da7f9cea7
Jinbuhm Kim <jinbuhm.kim@gmail.com>
2018-04-26 20:05:14 +0900

    blink, printf test

---
+++ test_uart/MDK-ARM/RTE/RTE_Components.h
@@ -0,0 +1,14 @@
+
+/*
+ * Auto generated Run-Time-Environment Component Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'test_uart' 
+ * Target:  'test_uart' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+#endif /* RTE_COMPONENTS_H */
